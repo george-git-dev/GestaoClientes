073a0e0874019401026f933f21963cd286935300
George Silva <george_roots@hotmail.com>
2023-03-25 21:38:41 -0300

    mapeando entidades

---
+++ src/main/java/clientes/ServicoRepository.java
@@ -0,0 +1,7 @@
+package clientes;
+
+import clientes.model.entity.Servico;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServicoRepository extends JpaRepository<Servico, Integer> {
+}
