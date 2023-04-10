061c2313280920ae7654c24dec044b4d093547df
George Silva <george_roots@hotmail.com>
2023-04-09 22:41:57 -0300

    Resolvendo problema do Cors

--- src/main/java/clientes/rest/ClienteController.java
+++ src/main/java/clientes/rest/ClienteController.java
@@ -5,9 +5,11 @@ import clientes.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/clientes")
+@CrossOrigin("http://localhost:4200")
 public class ClienteController {
 
     @Autowired
@@ -18,6 +20,13 @@ public class ClienteController {
     @ResponseStatus(HttpStatus.CREATED)
     public Cliente salvar(@RequestBody Cliente cliente) {
         return repository.save(cliente);
+    }
+
+    @GetMapping("{id}")
+    public Cliente findById(@PathVariable Integer id) {
+        return repository
+                .findById(id)
+                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
 }
